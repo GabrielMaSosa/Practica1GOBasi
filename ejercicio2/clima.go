a42a6a44318e57d60093302cc75a45edb904c0df
GabrielMaSosa <142988457+GabrielMaSosa@users.noreply.github.com>
2023-08-29 23:32:57 -0300

    Ejercicio2

---
+++ ejercicio2/clima.go
@@ -0,0 +1,24 @@
+package main
+import "fmt"
+
+func main(){
+	var(
+		ubicacion string
+		temp float64
+		hum float64
+		presion float64
+
+	)
+	ubicacion="Obera Misiones"
+	temp=17.3
+	hum=96.3
+	presion= 1.01
+
+
+
+	fmt.Println("Ubicacion:",ubicacion," Temperatura:",temp,"°C "," Humedad:",hum, "% ", "Presion Atmosferica:", presion, "Atmosferas")
+
+
+
+
+}
